9506c7ec1c1112411b878a955612cca4c76086fc
Lucas Hlongwane <LucasHlongwane2014@outlook.com>
2019-05-09 19:10:05 +0200

    Add project files.

---
+++ EmailConfirm/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmailConfirm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
